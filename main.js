de6cbdd2778350830e1d8f9d6c5cca6f28dfefe1
SHUBHTHAREJA <shubhthareja12@gmail.com>
2021-06-07 20:47:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,19 +2,20 @@ img=""
 status=""
 objects=[];
 function preload(){
-    img= loadImage("dog_cat.jpg")
+
 }
 
 function setup(){
  canvas=createCanvas(550,450);
  canvas.center();
+ video=createCapture(VIDEO);
+ video.hide();
  Objectdectector=ml5.objectDetector("cocossd",modelLoaded);
  document.getElementById("status").innerHTML="status detecting";
 }
 function modelLoaded(){
     console.log("model loaded");
     status=true;
-    Objectdectector.detect(img,gotResults);
 }
 
 function gotResults(error,results){
@@ -27,19 +28,23 @@ function gotResults(error,results){
 }
 
 function draw(){
-    image(img,0,0,550,450);
+    image(video,0,0,550,450);
     
     if (status != "") {
         
         console.log(status);
+        Objectdectector.detect(video,gotResults);
          for ( var i = 0; i < objects.length; i++) {
             document.getElementById("status").innerHTML="status detected";
-
-            fill("red");
+            document.getElementById("objects").innerHTML="there are " + objects.length + " " + "object"; 
+            r = random(250);
+            g = random(250);
+            b = random(250);
+            fill(r,g,b);
             percantange= floor(objects[i].confidence * 100);
              text(objects[i].label + " " + percantange + "%" , objects[i].x + 15,objects[i].y + 15);
              noFill();
-             stroke("red");
+             stroke(r,g,b);
              rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
          }
     }
